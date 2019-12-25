c606fffca8456cffc81a7db17d33a00f174f8a8a
咩咩 <oncwnuLCyaO7t7aE98I_-mBLkLiw@git.weixin.qq.com>
2019-12-25 16:45:34 +0800

    Initial commit

---
+++ src/main/java/com/noobz/iwant/IwantApplication.java
@@ -0,0 +1,18 @@
+package com.noobz.iwant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ * @author zhousz
+ * @date 2019/12/25  12:26
+ */
+@SpringBootApplication
+public class IwantApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(IwantApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/noobz/iwant/IwantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noobz.iwant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IwantApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
